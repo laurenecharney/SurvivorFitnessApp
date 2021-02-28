bf0418e509e06325e7bb20167809ce864f21a235
Alicia Yang <alicia.z.yang@vanderbilt.edu>
2021-02-28 17:14:23 -0600

    Added save/edit to trainer and dietician session pages

--- Pages/TrainerSession.js
+++ Pages/TrainerSession.js
@@ -2,9 +2,24 @@ import React, {Component} from 'react';
 import { StyleSheet, TextInput, View, Button, SafeAreaView, Text, ListViewComponent} from 'react-native';
 import Sidebar from '../Components/Sidebar.js';
 import DateTextBox from '../Components/DateTextBox.js';
-import NotesTextBox from '../Components/NotesTextBox.js'
+import MultilineInputSaveComponent from '../Components/MultilineInputSaveComponent'
 
 export default class TrainerSession extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        trainerNotes: "",
+        edit: false
+
+    }
+    if (Platform.OS === 'android') {
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+}
+changeText = (newValue)=>{
+    this.setState({trainerNotes: newValue});
+}
   render() {
     return (
       <View style={{flexDirection: 'row', left: 10, position: 'relative',flex: 2}}>
@@ -14,17 +29,29 @@ export default class TrainerSession extends Component {
             <Text style={{fontSize: 25}}>Session 6</Text>
           </SafeAreaView>
           <DateTextBox/>
-          <NotesTextBox name={"Trainer"}/>
-          <Text style={{fontSize: 10, padding: 5, width: '90%', marginLeft: 30}}>
-            *If needed, please contact ____ with any concerns or questions.
-          </Text>
-          <NotesTextBox name = {"Admin"}/>
-          <View>
-              <Button title="SAVE"/>
-          </View>
-          <View>
-              <Button title="EDIT"/>
-          </View>
+          <MultilineInputSaveComponent
+                        edit={this.state.edit}
+                        value={this.state.trainerNotes}
+                        placeholder = "Record Routine, exercise reps ... "
+                        changeText = {newValue => this.changeText(newValue)}
+                        heading = "Trainer Notes"
+                    />
+                    
+                <Text style={{fontSize: 10, padding: 10,margin:10}}>
+                    *If needed, please contact ____ with any concerns or questions.
+                    </Text>
+                <MultilineInputSaveComponent
+                        edit={false}
+                        value={"Lorem Impsum dolor"}
+                        placeholder = ""
+                        changeText = {newValue => this.changeText(newValue)}
+                        heading = "Admin Notes"
+                    />
+          <Button 
+                        title = {this.state.edit ? "SAVE" : "EDIT"}
+                        onPress={()=>this.setState({edit: !this.state.edit})}
+                        color={'black'}
+          />  
         </View>
       </View>
     );
