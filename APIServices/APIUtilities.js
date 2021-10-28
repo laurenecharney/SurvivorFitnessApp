a48c515b21e52904f260e266d2780ef8d66e1ddc
Ethan Shifrin <Ethan.shifrin@vanderbilt.edu>
2021-10-27 20:34:41 -0500

    small changes

--- APIServices/APIUtilities.js
+++ APIServices/APIUtilities.js
@@ -3,7 +3,7 @@ import { getItem } from "./deviceStorage";
 //this should be an env. variable. Fix this later
 
 //put
-// const ENDPOINT = "http://10.0.0.222:8080"; //convent
+const ENDPOINT = "http://10.0.0.222:8080"; //convent
 // const ENDPOINT = "http://10.76.31.229:8080"; //vuNet - fgh
 // const ENDPOINT = "http://10.66.190.160:8080"; //vuNet - rand
 // const ENDPOINT = "http://192.168.10.85:8080"; //Fido
