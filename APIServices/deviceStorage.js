8fcd255954eb20ca4cca52eddff3c1f65279d7b4
bengant3 <bengant3@gmail.com>
2022-01-10 16:11:52 -0500

    changed this package reference to reflect the new package name

--- APIServices/deviceStorage.js
+++ APIServices/deviceStorage.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from "react-native";
+import { AsyncStorage } from "@react-native-async-storage/async-storage";
 import * as SecureStore from "expo-secure-store";
 
 export async function saveItem(key, value) {
